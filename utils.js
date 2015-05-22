13c0cb93dd8d6966b80277700185207485a29f75
Paolo Cavanna <paolo_cavanna@hotmail.com>
2015-05-22 15:53:10 +0200

    Deleted backbone-like Object class

--- utils.js
+++ utils.js
@@ -620,64 +620,6 @@ MYAPP._EVENTS_ = {
 };
 
 /**
- * [description]
- * @param  {[type]} $    [description]
- * @param  {[type]} root [description]
- * @example
- * var myFn = app.Object.extend({
-		foo: "bar"
-	});
- */
-(function($, root) {
-	var hasOwnProperty = Object.prototype.hasOwnProperty;
-
-	function has(obj, key) {
-		return obj != null && hasOwnProperty.call(obj, key);
-	}
-
-	function extend(protoProps, staticProps) {
-		var parent = this;
-		var child;
-
-		// The constructor function for the new subclass is either defined by you
-		// (the "constructor" property in your `extend` definition), or defaulted
-		// by us to simply call the parent's constructor.
-		if (protoProps && has(protoProps, 'constructor')) {
-			child = protoProps.constructor;
-		} else {
-			child = function(){ return parent.apply(this, arguments); };
-		}
-
-		// Add static properties to the constructor function, if supplied.
-		$.extend(child, parent, staticProps);
-
-		// Set the prototype chain to inherit from `parent`, without calling
-		// `parent`'s constructor function.
-		var Surrogate = function(){ this.constructor = child; };
-		Surrogate.prototype = parent.prototype;
-		child.prototype = new Surrogate;
-
-		// Add prototype properties (instance properties) to the subclass,
-		// if supplied.
-		if (protoProps) $.extend(child.prototype, protoProps);
-
-		// Set a convenience property in case the parent's prototype is needed
-		// later.
-		child.__super__ = parent.prototype;
-
-		return child;
-	}
-
-	function Object() {
-		this.initialize.apply(this, arguments);
-	}
-
-	Object.extend = extend;
-
-	root.Object = Object;
-}(jQuery, MYAPP));
-
-/**
 * Utility to manage cookies
 * @namespace MYAPP
 * @class docCookies
