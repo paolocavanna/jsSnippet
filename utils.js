ad555a6309194307963a4266f6c8eb8378f306c5
Paolo Cavanna <paolo_cavanna@hotmail.com>
2015-08-06 10:19:16 +0200

    Improved template engine

--- utils.js
+++ utils.js
@@ -431,34 +431,46 @@ MYAPP.HtmlTemplates = (function(app) {
 	 */
 	_set = function(templateName, templateData) {
 
-		var html = _get(templateName),
-			re = /<%([^%>]+)?%>/g,
-			reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
-			code = 'var r=[];\n',
+		var html = _get(templateName) || templateName,
+			re = /<%(.+?)%>/g,
+			reExp = /(^( )?(var|if|for|else|switch|case|break|{|}|;))(.*)?/g,
+			code = 'with(obj) { var r=[];\n',
 			cursor = 0,
 			match,
-			add;
+			result;
 
-		add = function(line, js) {
+		var add = function(line, js) {
 
-			js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
-
-				(code += line !== '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+			js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+				(code += line !== "" ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
 
 			return add;
 
 		};
 
-		while (match = re.exec(html)) {
+		while(match = re.exec(html)) {
+
 			add(html.slice(cursor, match.index))(match[1], true);
+
 			cursor = match.index + match[0].length;
+
 		}
 
 		add(html.substr(cursor, html.length - cursor));
 
-		code += 'return r.join("");';
+		code = (code + 'return r.join(""); }').replace(/[\r\t\n]/g, '');
 
-		return new Function(code.replace(/[\r\t\n]/g, '')).apply(templateData);
+		try {
+
+			result = new Function('obj', code).apply(templateData, [templateData]);
+
+		} catch(err) {
+
+			console.error("'" + err.message + "'", " in \n\nCode:\n", code, "\n");
+
+		}
+
+		return result;
 
 	};
 
